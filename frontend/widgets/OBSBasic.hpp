1dee6fc431eb1060ba2ef9c198b32aa355257f45
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-06-09 22:55:38 +0200

    frontend: Add standalone SetCurrentSceneCollection

--- frontend/widgets/OBSBasic.hpp
+++ frontend/widgets/OBSBasic.hpp
@@ -1056,7 +1056,7 @@ private:
 
 	bool CreateDuplicateSceneCollection(const QString &name);
 	void DeleteSceneCollection(const QString &name);
-	void ChangeSceneCollection();
+	void SceneCollectionActionTriggered();
 
 	void RefreshSceneCollectionCache();
 
@@ -1090,6 +1090,7 @@ public:
 	std::vector<std::reference_wrapper<OBS::SceneCollection>> GetSceneCollectionsSorted() noexcept;
 
 	SceneCollection &GetCurrentSceneCollection();
+	void SetCurrentSceneCollection(SceneCollection &collection);
 
 	std::optional<SceneCollection> GetSceneCollectionByName(const std::string &collectionName) const;
 	std::optional<SceneCollection> GetSceneCollectionByFileName(const std::string &fileName) const;

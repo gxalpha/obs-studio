4a8b521b7ef4fffe65038e1ccf8c9c682cc52e50
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-06-09 19:46:13 +0200

    frontend: Add method to get sorted scene collections

--- frontend/widgets/OBSBasic.hpp
+++ frontend/widgets/OBSBasic.hpp
@@ -1087,6 +1087,7 @@ public:
 	inline bool SavingDisabled() const { return disableSaving; }
 
 	inline const OBSSceneCollectionCache &GetSceneCollectionCache() const noexcept { return collections; };
+	std::vector<std::reference_wrapper<OBS::SceneCollection>> GetSceneCollectionsSorted() noexcept;
 
 	SceneCollection &GetCurrentSceneCollection();
 
